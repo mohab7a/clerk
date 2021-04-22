0363e55d25a1c932ae80b51357efe8b584a66ee8
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2021-04-22 11:51:36 +0200

    finish profile screen3

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,6 @@ Future<void> main() async {
         SignUpScreen.id: (context) => SignUpScreen(),
         NavigationBar.id: (context) => NavigationBar(),
         HomeScreen.id: (context) => HomeScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
         ProfileScreen.id: (context) => ProfileScreen()
       },
     ),
