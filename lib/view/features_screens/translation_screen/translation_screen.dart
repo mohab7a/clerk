73f95bff1a1c877b089b2e7a3b3ff4faba7e84e6
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2021-05-02 00:11:18 +0200

    translation google

--- lib/view/features_screens/translation_screen/translation_screen.dart
+++ lib/view/features_screens/translation_screen/translation_screen.dart
@@ -4,89 +4,99 @@ import 'package:flutter/material.dart';
 import '../../../constants.dart';
 
 class TranslationScreen extends StatefulWidget {
-
   @override
   _TranslationScreenState createState() => _TranslationScreenState();
 }
 
 class _TranslationScreenState extends State<TranslationScreen> {
-
   TextEditingController inputField = TextEditingController();
   TextEditingController outputField = TextEditingController();
 
   // @override
   // void didChangeDependencies() {
-  //   translate(text: inputField.text,toLanguage: 'ar');
+  //
   //   super.didChangeDependencies();
   // }
-   @override
+  @override
   Widget build(BuildContext context) {
     return NotificationListener<OverscrollIndicatorNotification>(
-      onNotification: (overScroll){
+      onNotification: (overScroll) {
         overScroll.disallowGlow();
         return;
       },
       child: FutureBuilder(
-        future: translate(text: inputField.text,toLanguage: 'ar'),
-        builder: (context, snapshot) {
-           return SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height*0.01,),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                  margin: EdgeInsets.only(left: 20, right: 20, bottom: 20,),
-                  decoration: kCustomBoxDecoration,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextFormField(
-                        controller: inputField,
-                        onChanged: (value){
-                          setState(() {
-                            outputField.text = snapshot.data[0]['translations'][0]['text'];
-                          });
-                        },
-                        maxLines: null,
-                        minLines: 11,
-                        showCursor: true,
-                        cursorColor: kPrimaryColor,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            hintText: 'Enter Text Here'),
-                      ),
-                    ],
+          future: translate(text: inputField.text, toLanguage: 'ar'),
+          builder: (context, snapshot) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                  margin: EdgeInsets.only(left: 20, right: 20, bottom: 20,),
-                  decoration: kCustomBoxDecoration,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextFormField(
-                        controller: outputField,
-                        readOnly: true,
-                        maxLines: null,
-                        minLines: 10,
-                        showCursor: false,
-                        cursorColor: kPrimaryColor,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            hintText: 'Your Output '),
-                      ),
-                      OptionsRow(),
-                    ],
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      bottom: 20,
+                    ),
+                    decoration: kCustomBoxDecoration,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextFormField(
+                          controller: inputField,
+                          onChanged: (value) {
+                            setState(() {
+                              translate(
+                                  text: inputField.text, toLanguage: 'ar');
+                              outputField.text = snapshot.data["data"]
+                                  ["translations"][0]["translatedText"];
+                            });
+                          },
+                          maxLines: null,
+                          minLines: 11,
+                          showCursor: true,
+                          cursorColor: kPrimaryColor,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: 'Enter Text Here'),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          );
-        }
-      ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      bottom: 20,
+                    ),
+                    decoration: kCustomBoxDecoration,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextFormField(
+                          controller: outputField,
+                          readOnly: true,
+                          maxLines: null,
+                          minLines: 10,
+                          showCursor: false,
+                          cursorColor: kPrimaryColor,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: 'Your Output '),
+                        ),
+                        OptionsRow(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
--- lib/view_model/translation_api.dart
+++ lib/view_model/translation_api.dart
@@ -2,20 +2,34 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+// Future<dynamic> translate({String text, String toLanguage}) async {
+//   final response = await http.post(
+//     Uri.https('microsoft-translator-text.p.rapidapi.com', 'translate',
+//         {"to": toLanguage, "api-version": "3.0"}),
+//     headers: <String, String>{
+//       'Content-Type': 'application/json; charset=UTF-8',
+//       "x-rapidapi-key": "bd9d9da516msh92534814667ad69p11f0d5jsn2b6479a0bd43",
+//     },
+//     body: jsonEncode([
+//       {"text": text}
+//     ]),
+//   );
+//   if (response.statusCode == 200) {
+//     List<dynamic> data = json.decode(response.body);
+//     return data;
+//   } else {
+//     throw (Exception);
+//   }
+// }
+
 Future<dynamic> translate({String text, String toLanguage}) async {
   final response = await http.post(
-    Uri.https('microsoft-translator-text.p.rapidapi.com', 'translate',
-        {"to": toLanguage, "api-version": "3.0"}),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-      "x-rapidapi-key": "bd9d9da516msh92534814667ad69p11f0d5jsn2b6479a0bd43",
-    },
-    body: jsonEncode([
-      {"text": text}
-    ]),
+    Uri.parse(
+        "https://translation.googleapis.com/language/translate/v2?key=AIzaSyAxHcXzqV6YWKJZ9i5VKgmm1IPlJjy5hfc&q=$text&target=$toLanguage"),
   );
   if (response.statusCode == 200) {
-    List<dynamic> data = json.decode(response.body);
+    Map<String, dynamic> data = json.decode(response.body);
+    print(data["data"]["translations"][0]["translatedText"]);
     return data;
   } else {
     throw (Exception);
